62842b058ff0e5e1fd9c34655fbfc789f286b58b
Atique Akhtar <atiqueakhtar786@gmail.com>
2022-02-08 00:10:07 +0530

    Refactor: Retweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,8 +6,6 @@ class Tweet < ApplicationRecord
     # validates :body, presence: true, length: { maximum: 100 }
     has_many :retweets, class_name: "Tweet", foreign_key: "parent_id", dependent: :destroy
     has_many :retweeted_by, through: :retweets, class_name: "User", source: :user
-    # scope :retweeted?, ->(parent_id) { where(user_id: Current.user.id).where(parent_id: parent_id) }
-    # scope :retweets, ->(parent_id) { where(parent_id: parent_id) }
 
     def liked_by?(user_id)
         self.liked_by.pluck(:id).include?(user_id)
