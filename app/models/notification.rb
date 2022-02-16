84686e1695b2ea0bf446649d0e2caaf38a69fb7e
Atique Akhtar <atiqueakhtar786@gmail.com>
2022-02-16 18:53:41 +0530

    Refactor: Notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,11 +2,4 @@ class Notification < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :notifiable, polymorphic: true
 
-  def create(tweet)
-    create_notification(tweet)
-  end
-  private
-    def create_notification(tweet)
-      Notification.create(notifiable: self, tweet_user_id: tweet.user_id)
-    end
 end
