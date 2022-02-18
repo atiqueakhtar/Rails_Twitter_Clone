c2d4e9ab1325c53cec84d8b5efbbc6e8c04be256
Atique Akhtar <atiqueakhtar786@gmail.com>
2022-02-18 16:48:36 +0530

    remove debugger

--- app/jobs/create_notification_job.rb
+++ app/jobs/create_notification_job.rb
@@ -2,10 +2,9 @@ class CreateNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(action_id, action_type)
-    debugger
     action = action_type.constantize.find_by(id: action_id)
     return unless action  
-        
+
     action_tweet = action.try(:tweet) || action.try(:parent_tweet)
 
     Notification.create(notifiable: action, notifier_id: action_tweet.user_id)
